f9cb09abcaf687625d43ef4e8d126b6133b59dd8
mrhihihi <gabriel.kyeremateng.gk@gmail.com>
2025-04-11 16:20:10 +0000

    Created the endpoints

--- controllers/products.js
+++ controllers/products.js
@@ -1,20 +1,34 @@
 import { ProductModel } from "../models/products.js";
+import { addProductValidator } from "../validators/products.js";
 
+export const addProduct = async (req, res, next) => {
+    // Validate incoming req from body
+  const { error, value } = addProductValidator.validate({
+    ...req.body,
+    image: req.file?.filename,
+  });
 
-export const addProduct = (req,res,next) => {
-    
-}
+  if (error) {
+    return res.status(422).json(error);
+  }
+
+  // Create a product and save in database.
+  const product = await ProductModel.create(value);
+
+  res.status(201).json(product);
+};
+
 export const getProducts = (req, res) => {
   const products = ProductModel.find();
 
   return res.status(200).json(products);
 };
 
-export const getProduct = (req, res, next) => {
+export const getProduct = async (req, res, next) => {
   try {
     const productId = req.params.id;
 
-    const product = ProductModel.findById(productId);
+    const product = await ProductModel.findById(productId);
 
     if (product) {
       return res.status(200).json(product);
@@ -26,34 +40,32 @@ export const getProduct = (req, res, next) => {
   }
 };
 
-export const updateProduct = (req, res, next) => {
+export const updateProduct = async (req, res, next) => {
   try {
     const productId = req.params.id;
-  
-    const product = ProductModel.findByIdAndUpdate(productId, req.body, {
+
+    const product = await ProductModel.findByIdAndUpdate(productId, req.body, {
       new: true,
     });
-  
-    if(product)
-    {
-      return res.status(200).json('Product successfully updated.')
+
+    if (product) {
+      return res.status(200).json("Product successfully updated.");
     }
-  
-    return res.status(400).json('Product not found.');
+
+    return res.status(400).json("Product not found.");
   } catch (error) {
     next(error);
   }
 };
 
-export const deleteProduct = (req,res, next) => {
-    const productId = req.params.id;
+export const deleteProduct = async (req, res, next) => {
+  const productId = req.params.id;
 
-    const product = ProductModel.findByIdAndDelete(productId);
+  const product = await ProductModel.findByIdAndDelete(productId);
 
-    if(product)
-    {
-        return res.status(200).json('Product successfully deleted.');
-    }
+  if (product) {
+    return res.status(200).json("Product successfully deleted.");
+  }
 
-    return res.status(403).json('Product not found');
-}
+  return res.status(403).json("Product not found");
+};
---
+++ routes/products.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import { addProduct, deleteProduct, getProduct, getProducts, updateProduct } from "../controllers/products";
+
+const productRouter = Router();
+
+productRouter.post("/products", productImageUpload.single("image"), addProduct);
+productRouter.get("/products/:id", getProduct);
+productRouter.get("/products",getProducts);
+productRouter.patch("/products/:id", updateProduct);
+productRouter.delete("/products/:id", deleteProduct);
+
+export default productRouter;
